bc51ee544df2b5525ab0b2fc97737c9d2bbe97d7
nusuke <nusuke@nusukenoMac-Studio.local>
2024-02-26 22:13:05 +0900

    refacotr

--- src/app/feature/jq/queryInput.tsx
+++ src/app/feature/jq/queryInput.tsx
@@ -1,20 +1,18 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { useDebounce } from "./useDebuonce";
 
 type P = {};
 export const QueryInput: React.FC<P> = () => {
   const [jqQuery, setJqQuery] = useState(".");
+  const debouncedInputText = useDebounce(jqQuery, 200);
 
   useEffect(() => {
-    console.log(jqQuery);
-    chrome.runtime
-      .sendMessage({
-        type: "query",
-        text: jqQuery,
-      })
-      .then((res) => {
-        console.log(res);
-      });
-  }, [jqQuery]);
+    chrome.runtime.sendMessage({
+      type: "query",
+      text: jqQuery,
+    });
+  }, [debouncedInputText]);
+
   return (
     <>
       <input
--- src/background.ts
+++ src/background.ts
@@ -6,47 +6,45 @@ type MessageType = {
 };
 const key = "json";
 
-chrome.runtime.onMessage.addListener(
-  async (message: MessageType, sender, sendResponse) => {
-    console.log("message 受信", message);
-    if (!sender.tab?.id) return;
+chrome.runtime.onMessage.addListener(async (message: MessageType, sender) => {
+  console.log("message 受信", message);
+  if (!sender.tab?.id) return;
 
-    if (message.type == "road") {
-      const text = message.text;
-      try {
-        const json = JSON.parse(text);
+  if (message.type == "road") {
+    const text = message.text;
+    try {
+      const json = JSON.parse(text);
 
-        await chrome.scripting.insertCSS({
-          files: ["style.css"],
-          target: { tabId: sender.tab.id },
-        });
+      await chrome.scripting.insertCSS({
+        files: ["style.css"],
+        target: { tabId: sender.tab.id },
+      });
 
-        await chrome.storage.session.set({ [key]: JSON.stringify(json) });
-      } catch (e) {
-        console.error(e);
-      }
-    } else if (message.type == "query") {
-      // jq filtering
-      const jqQuery = message.text;
-      try {
-        const json = JSON.parse(
-          JSON.parse(JSON.stringify(await chrome.storage.session.get(key))).json
-        );
+      await chrome.storage.session.set({ [key]: JSON.stringify(json) });
+    } catch (e) {
+      console.error(e);
+    }
+  } else if (message.type == "query") {
+    // jq filtering
+    const jqQuery = message.text;
+    try {
+      const json = JSON.parse(
+        JSON.parse(JSON.stringify(await chrome.storage.session.get(key))).json
+      );
 
-        const res = jq.json(json, jqQuery);
-        console.log("結果::", res);
+      const res = jq.json(json, jqQuery);
+      console.log("jq result::", res);
 
-        const [tab] = await chrome.tabs.query({
-          active: true,
-          lastFocusedWindow: true,
-        });
-        if (!tab.id) return;
-        chrome.tabs.sendMessage(tab.id, {
-          filteredJSON: res,
-        });
-      } catch (e) {
-        console.log(e);
-      }
+      const [tab] = await chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true,
+      });
+      if (!tab.id) return;
+      chrome.tabs.sendMessage(tab.id, {
+        filteredJSON: res,
+      });
+    } catch (e) {
+      console.log(e);
     }
   }
-);
+});
